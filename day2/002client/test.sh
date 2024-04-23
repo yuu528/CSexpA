9c9b88d06fdd6835f3c1c23fd02216ed775f251f
Yuu528 <yuu528mail@gmail.com>
2024-04-23 16:06:00 +0900

    day2/002: test.sh: Add sleep

--- day2/002client/test.sh
+++ day2/002client/test.sh
@@ -8,12 +8,14 @@ OUT="./client.test.csv"
 GRAPH_OUT="./client.test.png"
 
 MAX_EXP=18
+SLEEP=5
 
 echo "Buffer size,Time" > $OUT
 
 for i in $(seq 0 $MAX_EXP | xargs -n1 echo "2 ^" | bc); do
 	echo "Testing buffer size $i"
 	$EXE $TARGET_IP $TARGET_FILE $i >> $OUT
+	sleep $SLEEP
 done
 
 gnuplot <<EOF
--- day2/002server/test.sh
+++ day2/002server/test.sh
@@ -6,12 +6,14 @@ OUT="./server.test.csv"
 GRAPH_OUT="./server.test.png"
 
 MAX_EXP=18
+SLEEP=3
 
 echo "Buffer size,Time" > $OUT
 
 for i in $(seq 0 $MAX_EXP | xargs -n1 echo "2 ^" | bc); do
 	echo "Testing buffer size $i"
 	$EXE $i >> $OUT
+	sleep $SLEEP
 done
 
 gnuplot <<EOF
