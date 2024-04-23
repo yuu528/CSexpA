79cabe88171ae492b4b7d0a21a831aaa33dc6150
Yuu528 <yuu528mail@gmail.com>
2024-04-23 15:40:31 +0900

    day2/002: Add test scripts

---
+++ day2/002client/test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+EXE="./002client.out"
+TARGET_IP="192.168.1.101"
+TARGET_FILE="../100MB.txt"
+
+OUT="./client.test.csv"
+GRAPH_OUT="./client.test.png"
+
+MAX_EXP=18
+
+echo "Buffer size,Time" > $OUT
+
+for i in $(seq 0 $MAX_EXP | xargs -n1 echo "2 ^" | bc); do
+	$EXE $TARGET_IP $TARGET_FILE $i >> $OUT
+done
+
+gnuplot <<EOF
+set terminal pngcairo
+set output "$GRAPH_OUT"
+
+set mono
+
+set datafile separator ","
+
+set logscale x 2
+
+plot "$OUT" using 1:2 with linespoints pt 7 notitle
+EOF
---
+++ day2/002server/test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+EXE="./002server.out"
+TARGET_FILE="../100MB.txt"
+
+OUT="./server.test.csv"
+GRAPH_OUT="./server.test.png"
+
+MAX_EXP=18
+
+echo "Buffer size,Time" > $OUT
+
+for i in $(seq 0 $MAX_EXP | xargs -n1 echo "2 ^" | bc); do
+	$EXE $i >> $OUT
+done
+
+gnuplot <<EOF
+set terminal pngcairo
+set output "$GRAPH_OUT"
+
+set mono
+
+set datafile separator ","
+
+set logscale x 2
+
+plot "$OUT" using 1:2 with linespoints pt 7 notitle
+EOF
