97ce587d701c95b3935423e8af285f62c171cac2
Yuu528 <yuu528mail@gmail.com>
2024-04-23 15:35:41 +0900

    day2/001: test.sh: Add gnuplot routine

--- day2/001/test.sh
+++ day2/001/test.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
 EXE="./001.out"
-TARGET_FILE="../10MB.txt"
+TARGET_FILE="../100MB.txt"
 
 OUT="./data.test.csv"
+GRAPH_OUT="./data.test.png"
 
-MAX_EXP=20
+MAX_EXP=18
 
 echo "Buffer size,Time" > $OUT
 
-for i in $(seq 0 20 | xargs -n1 echo "2 ^" | bc); do
+for i in $(seq 0 $MAX_EXP | xargs -n1 echo "2 ^" | bc); do
 	$EXE $TARGET_FILE $i >> $OUT
 done
+
+gnuplot <<EOF
+set terminal pngcairo
+set output "$GRAPH_OUT"
+
+set mono
+
+set datafile separator ","
+
+set logscale x 2
+
+plot "$OUT" using 1:2 with linespoints pt 7 notitle
+EOF
