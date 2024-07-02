21db5d1fe820fa1e1d9048b85a1e54f876e8a348
Yuu528 <yuu528mail@gmail.com>
2024-07-02 14:50:14 +0900

    part3: zipsearch: Use ajax to get result

--- part3/zipsearch/index.js
+++ part3/zipsearch/index.js
@@ -1,25 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
 	const btn = document.getElementById('btn-submit');
-	const form = document.getElementById('form');
-
-	btn.addEventListener('click', e => {
-		form.submit();
-
-		return false;
-	});
-
-	const frame = document.getElementById('frame');
-	frame.addEventListener('load', e => {
-		frame.setAttribute('height', frame.contentWindow.document.documentElement.scrollHeight);
-	});
-
 	const input = document.getElementById('input-query');
 
+	btn.addEventListener('click', e => {
+		if(input.value.length == 0) return;
+		search(1);
+		return;
+	});
+
+	// Setup autocompletion
 	M.Autocomplete.init(document.querySelectorAll('.autocomplete'), {data: {}});
 	const acInstance = M.Autocomplete.getInstance(input);
 
 	const xhr = new XMLHttpRequest();
-	let toId = null;
 
 	xhr.addEventListener('load', e => {
 		if(xhr.status === 200) {
@@ -44,11 +37,154 @@ document.addEventListener('DOMContentLoaded', () => {
 	});
 
 	input.addEventListener('keyup', e => {
-		clearTimeout(toId);
-		toId = setTimeout(() => {
-			xhr.abort();
-			xhr.open('GET', `suggest.php?query=${input.value}`);
+		xhr.abort();
+
+		if(e.key === 'Enter') {
+			search(1);
+			acInstance.close();
+		} else {
+			xhr.open('GET', `result.php?query=${input.value}`);
 			xhr.send();
-		});
+		}
 	});
 });
+
+function search(page) {
+	const input = document.getElementById('input-query');
+	if(input.value.length == 0) return;
+
+	const xhr = new XMLHttpRequest();
+	xhr.addEventListener('load', e => {
+		if(xhr.status === 200) {
+			try {
+				const data = JSON.parse(xhr.responseText);
+				if(
+					!'results' in data ||
+					!'count' in data ||
+					!'perPage' in data ||
+					!'page' in data
+				) return;
+
+				// Add data to table
+				const tBody = document.getElementById('table-result-body');
+				tBody.innerText = '';
+
+				data.results.forEach(datum => {
+					/*
+					 * <tbody>
+					 *     <tr>
+					 *         <td>
+					 *              <ruby>
+					 *                  {addr1} <rt>{kana1}</rt>
+					 *                  ...
+					 *              </ruby>
+					 *         </td>
+					 *     </tr>
+					 * </tbody>
+					 */
+					const newTr = document.createElement('tr');
+					const newTdAddr = document.createElement('td');
+					const newTdZip = document.createElement('td');
+					const newRuby = document.createElement('ruby');
+
+					for(let i = 1; i <= 3; i++) {
+						const newText = document.createTextNode(datum[`addr${i}`]);
+						const newRt = document.createElement('rt');
+
+						newRt.innerText = datum[`kana${i}`];
+
+						newRuby.appendChild(newText);
+						newRuby.appendChild(newRt);
+					}
+
+					newTdZip.innerText = datum.zip.toString().padStart(7, '0').replace(/([0-9]{3})([0-9]{4})/, '$1-$2');
+
+					newTdAddr.appendChild(newRuby);
+					newTr.appendChild(newTdAddr);
+					newTr.appendChild(newTdZip);
+					tBody.appendChild(newTr);
+				});
+
+				const table = document.getElementById('table-result');
+				table.style.display = 'table';
+
+				// Show pagination
+				const pageUl = document.getElementById('pagination');
+				pageUl.innerText = '';
+
+				const lastPage = Math.ceil(data.count / data.perPage);
+
+				// prev btn
+				const newLiPrev = document.createElement('li');
+				const newAPrev = document.createElement('a');
+				const newIPrev = document.createElement('i');
+
+				// Materialize
+				if(data.page <= 1) {
+					newLiPrev.classList.add('disabled');
+				}
+				newIPrev.classList.add('material-icons');
+				newIPrev.innerText = 'chevron_left';
+
+				newAPrev.setAttribute('href', '#!');
+				newAPrev.addEventListener('click', () => {
+					search(data.page - 1);
+				});
+
+				newAPrev.appendChild(newIPrev);
+				newLiPrev.appendChild(newAPrev);
+				pageUl.appendChild(newLiPrev);
+
+				// pages btn
+				for(let i = 1; i <= lastPage; i++) {
+					const newLi = document.createElement('li');
+					const newA = document.createElement('a');
+
+					// Materialize
+					if(i === data.page) {
+						newLi.classList.add('active');
+					} else {
+						newLi.classList.add('waves-effect');
+					}
+
+					newA.setAttribute('href', '#!');
+					newA.innerText = i;
+					newA.addEventListener('click', () => {
+						search(i);
+					});
+
+					newLi.appendChild(newA);
+					pageUl.appendChild(newLi);
+				}
+
+				// prev btn
+				const newLiNext = document.createElement('li');
+				const newANext = document.createElement('a');
+				const newINext = document.createElement('i');
+
+				// Materialize
+				if(lastPage <= data.page) {
+					newLiNext.classList.add('disabled');
+				}
+				newINext.classList.add('material-icons');
+				newINext.innerText = 'chevron_right';
+
+				newANext.setAttribute('href', '#!');
+				newANext.addEventListener('click', () => {
+					search(data.page + 1);
+				});
+
+				newANext.appendChild(newINext);
+				newLiNext.appendChild(newANext);
+				pageUl.appendChild(newLiNext);
+
+				pageUl.style.display = 'block';
+			} catch(e) {
+				console.error(e);
+			}
+		}
+	});
+
+	xhr.open('GET', `result.php?query=${input.value}&page=${page}`);
+	xhr.send();
+}
--- part3/zipsearch/result.php
+++ part3/zipsearch/result.php
@@ -1,223 +1,98 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>ZipSearch 検索結果</title>
+<?php
+// constants
+const GET_QUERY = 'query';
+const GET_PAGE = 'page';
 
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+const DATA_RESULTS = 'results';
+const DATA_COUNT = 'count';
+const DATA_PER_PAGE = 'perPage';
+const DATA_PAGE = 'page';
 
-		<!-- Materialize -->
-		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+const DB_HOST = 'localhost';
+const DB_USER = 'pi';
+const DB_PASSWORD = 'csexpatestpassword';
+const DB_NAME = 'CSexp1DB';
+const DB_TABLE = 'zipJapan';
 
-		<link rel="stylesheet" href="style.css">
-	</head>
-	<body>
-		<?php
-		ini_set('display_errors', '1');
+const RESULTS_PER_PAGE = 10;
 
-		// constants
-		const GET_QUERY = 'query';
-		const GET_PAGE = 'page';
+function reply($data) {
+	header('Content-Type: application/json');
+	echo json_encode($data);
+	exit();
+}
 
-		const DB_HOST = 'localhost';
-		const DB_USER = 'pi';
-		const DB_PASSWORD = 'csexpatestpassword';
-		const DB_NAME = 'CSexp1DB';
-		const DB_TABLE = 'zipJapan';
+$data = [
+	'results' => [
+	]
+];
 
-		const RESULTS_PER_PAGE = 10;
+// parse params
+if(array_key_exists(GET_QUERY, $_GET)) {
+	// remove space and tab
+	$query = str_replace(
+		[' ', '\t'],
+		'',
+		$_GET[GET_QUERY]
+	);
+} else {
+	reply($data);
+}
 
-		const LANG_DEFAULT = 'ja';
+if(array_key_exists(GET_PAGE, $_GET) && is_numeric($_GET[GET_PAGE])) {
+	$page = intval($_GET[GET_PAGE]);
+} else {
+	$page = 1;
+}
 
-		const LANG_NO_QUERY = 'no_query';
-		const LANG_DB_CONNECTION_ERROR = 'db_connection_error';
-		const LANG_QUERY = 'query';
-		const LANG_PAGE = 'page';
-		const LANG_NINM_PAGE = 'ninm_page';
-		const LANG_NO_PAGE = 'no_page';
-		const LANG_N_ALL = 'n_all';
-		const LANG_ZIP = 'zip';
-		const LANG_ADDR = 'addr';
+// connect to DB
+$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+if(is_null($mysqli)) {
+	print_error(i18n(LANG_DB_CONNECTION_ERROR));
+}
 
-		function i18n($id) {
-			// translations
-			static $langs = [
-				'ja' => [
-					LANG_NO_QUERY => '検索値が指定されていません。',
-					LANG_DB_CONNECTION_ERROR => 'データベース接続エラー。',
-					LANG_QUERY => '検索値',
-					LANG_PAGE => 'ページ',
-					LANG_NINM_PAGE => '%d / %d ページ',
-					LANG_NO_PAGE => '指定されたページ番号のページはありません。',
-					LANG_N_ALL => '全%d件',
-					LANG_ZIP => '郵便番号',
-					LANG_ADDR => '住所'
-				]
-			];
+$query_like = "%$query%";
 
-			return $langs[LANG_DEFAULT][$id];
-		}
+if(is_numeric($query)) {
+	$q_where = 'WHERE zip LIKE ?';
+} elseif(preg_match('/^([0-9]*)-([0-9]*)$/', $query, $m) === 1 && count($m) == 3) {
+	// 123-4567 -> 123-4567
+	// 23-45 -> _23-45__
+	$query_like = str_repeat('_', 3 - strlen($m[1])) . $m[1] . $m[2] . str_repeat('_', 4 - strlen($m[2]));
+	$q_where = 'WHERE zip LIKE ?';
+} elseif(preg_match('/^[ァ-ヿ0-9()]+$/u', $query) === 1) {
+	$q_where = 'WHERE CONCAT(kana1, kana2, kana3) LIKE ?';
+} else {
+	$q_where = 'WHERE CONCAT(addr1, addr2, addr3) LIKE ?';
+}
 
-		function print_page_link($query, $page, $text, $class = 'waves-effect') {
-			echo '<li class="' . $class . '"><a href="result.php?query=' . urlencode($query) . '&page=' . $page . '">' . $text . '</a></li>';
-		}
+// set limits
+$from_row = RESULTS_PER_PAGE * ($page - 1);
+$to_row = RESULTS_PER_PAGE;
 
-		// print a message as an error and exit
-		function print_error($msg) {
-			echo $msg;
-			exit();
-		}
+// get result
+$sqlq = $mysqli->prepare('SELECT * FROM ' . DB_TABLE . " $q_where LIMIT ?, ?");
+$sqlq->bind_param('sii', $query_like, $from_row, $to_row);
+$sqlq->execute();
+$result = $sqlq->get_result();
 
-		// get params
-		if(array_key_exists(GET_QUERY, $_GET)) {
-			// remove space and tab
-			$query = str_replace(
-				[' ', '\t'],
-				'',
-				$_GET[GET_QUERY]
-			);
-		} else {
-			print_error(i18n(LANG_NO_QUERY));
-		}
+$sqlq->close();
 
-		if($query === '') {
-			print_error(i18n(LANG_NO_QUERY));
-		}
+// count all result
+$sqlq_count = $mysqli->prepare('SELECT count(*) FROM ' . DB_TABLE . " $q_where");
+$sqlq_count->bind_param('s', $query_like);
 
-		if(array_key_exists(GET_PAGE, $_GET) && is_numeric($_GET[GET_PAGE])) {
-			$page = intval($_GET[GET_PAGE]);
-		} else {
-			$page = 1;
-		}
+$sqlq_count->execute();
+$result_count = $sqlq_count->get_result();
+$data[DATA_COUNT] = $result_count->fetch_row()[0];
+$data[DATA_PER_PAGE] = RESULTS_PER_PAGE;
+$data[DATA_PAGE] = $page;
 
-		// connect to DB
-		$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-		if(is_null($mysqli)) {
-			print_error(i18n(LANG_DB_CONNECTION_ERROR));
-		}
+$sqlq_count->close();
 
-		// check if query is kana or zip
-		$query_like = "%$query%";
+while(!is_null($row = $result->fetch_assoc()) && $row !== false) {
+	array_push($data[DATA_RESULTS], $row);
+}
 
-		if(is_numeric($query)) {
-			$q_where = 'WHERE zip LIKE ?';
-		} elseif(preg_match('/^([0-9]*)-([0-9]*)$/', $query, $m) === 1 && count($m) == 3) {
-			// 123-4567 -> 123-4567
-			// 23-45 -> _23-45__
-			$query_like = str_repeat('_', 3 - strlen($m[1])) . $m[1] . $m[2] . str_repeat('_', 4 - strlen($m[2]));
-			$q_where = 'WHERE zip LIKE ?';
-		} elseif(preg_match('/^[ァ-ヿ0-9()]+$/u', $query) === 1) {
-			$q_where = 'WHERE CONCAT(kana1, kana2, kana3) LIKE ?';
-		} else {
-			$q_where = 'WHERE CONCAT(addr1, addr2, addr3) LIKE ?';
-		}
-
-		// set limits
-		$from_row = RESULTS_PER_PAGE * ($page - 1);
-		$to_row = RESULTS_PER_PAGE;
-
-		$sqlq = $mysqli->prepare('SELECT * FROM ' . DB_TABLE . " $q_where LIMIT ?, ?");
-
-		$sqlq->bind_param('sii', $query_like, $from_row, $to_row);
-
-		// get result
-		$sqlq->execute();
-		$result = $sqlq->get_result();
-
-		// count all result
-		$sqlq_count = $mysqli->prepare('SELECT count(*) FROM ' . DB_TABLE . " $q_where");
-		$sqlq_count->bind_param('s', $query_like);
-
-		$sqlq_count->execute();
-		$result_count = $sqlq_count->get_result();
-		$count = $result_count->fetch_row()[0];
-		$end_page = ceil($count / RESULTS_PER_PAGE);
-
-		$sqlq_count->close();
-		?>
-
-		<!-- Result count -->
-		<?php
-		printf(i18n(LANG_N_ALL), $count);
-
-		if($count <= $from_row || $from_row < 0) {
-			echo '<br>';
-			print_error(i18n(LANG_NO_PAGE));
-		}
-		?>
-		<span style="display: inline-block; width: 2em;"></span>
-		<?php
-		printf(i18n(LANG_NINM_PAGE), $page, $end_page);
-		?>
-
-		<!-- Result table -->
-		<table>
-			<thead>
-				<tr>
-					<?php
-					foreach([
-						i18n(LANG_ADDR),
-						i18n(LANG_ZIP)
-					] as $col) {
-						echo "<th>$col</th>";
-					}
-					?>
-				</tr>
-			</thead>
-			<tbody>
-				<?php
-				while(!is_null($row = $result->fetch_assoc()) && $row !== false) {
-					// print address with ruby (kana)
-					echo '<tr><td><ruby>';
-					foreach([
-						[ $row['addr1'], $row['kana1'] ],
-						[ $row['addr2'], $row['kana2'] ],
-						[ $row['addr3'], $row['kana3'] ]
-					] as $addr) {
-						echo "${addr[0]}<rt>${addr[1]}</rt>";
-					}
-
-					// print zip
-					$intzip = strval($row['zip']);
-					echo '</ruby></td><td>' . substr($intzip, 0, 3) . '-' . substr($intzip, -4, 4) . '</td></tr>';
-				}
-
-				$sqlq->close();
-				$mysqli->close();
-				?>
-			</tbody>
-		</table>
-
-		<!-- Pagenation -->
-		<ul class="pagination">
-			<?php
-			// prev
-			if(1 < $page) {
-				print_page_link($query, $page - 1, '<i class="material-icons">chevron_left</i>');
-			} else {
-				print_page_link($query, '', '<i class="material-icons">chevron_left</i>', 'disabled');
-			}
-
-			// pages
-			for($i = 1; $i <= $end_page; $i++) {
-				if($i === $page) {
-					print_page_link($query, $i, $i, 'active');
-				} else {
-					print_page_link($query, $i, $i);
-				}
-			}
-
-			// next
-			if($page < $end_page) {
-				print_page_link($query, $page + 1, '<i class="material-icons">chevron_right</i>');
-			} else {
-				print_page_link($query, '', '<i class="material-icons">chevron_right</i>', 'disabled');
-			}
-			?>
-		</ul>
-
-		<!-- Materialize -->
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-	</body>
-</html>
+reply($data);
+?>
